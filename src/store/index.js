e5695469d2c091ef4221f0fddb24fda580b4117a
moiseev.sliceplanet@gmail.com <moiseev.sliceplanet@gmail.com>
2019-12-03 21:56:38 +0200

    currenci

--- src/store/index.js
+++ src/store/index.js
@@ -20,6 +20,13 @@ export default new Vuex.Store({
   getters: {
     error: s => s.error
   },
+  actions: {
+    async fetchCurrency () {
+      const key = process.env.VUE_APP_FIXER
+      const res = await fetch(`http://data.fixer.io/api/latest?access_key=${key}&format=1&symbols=USD,EUR,RUB,UAH`)
+      return res.json()
+    }
+  },
   modules: {
     auth,
     info
